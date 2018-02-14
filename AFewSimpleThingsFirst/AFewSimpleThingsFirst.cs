0100193baef23394f579ee635785fb160149bdc9
GitInMaRepo <oliver.s.roth@gmail.com>
2018-02-14 13:48:14 +0100

    A quicksort in f sharp and pure c sharp

--- AFewSimpleThingsFirst/AFewSimpleThingsFirst.cs
+++ AFewSimpleThingsFirst/AFewSimpleThingsFirst.cs
@@ -1,6 +1,9 @@
 ﻿using FluentAssertions;
 using FSharpImplementations;
+using Microsoft.FSharp.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AFewSimpleThingsFirst
 {
@@ -8,13 +11,60 @@ namespace AFewSimpleThingsFirst
     public class AFewSimpleThingsFirst
     {
         [TestMethod]
-        public void TheSumOfAllSquaresFrom1To4()
+        public void TheSumOfAllSquaresFrom1To4In_CSharp()
         {
             var targetCSharp = new SquareCalculatorCSharp();
-            var targetFSharp = new SquareCalculatorFSharp();
-
             targetCSharp.SquaresFromOneTo(4).Should().Be(30);
+        }
+
+        [TestMethod]
+        public void TheSumOfAllSquaresFrom1To4In_FSharp()
+        {
+            var targetFSharp = new SquareCalculatorFSharp();
             targetFSharp.SquaresFromOneTo(4).Should().Be(30);
         }
+
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_CSharp()
+        {
+            var targetCSharp = new QuickSortCSharp();
+            targetCSharp
+                .DoQuickSortOn(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 })
+                .Should()
+                .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_CSharp_AlternateVersion()
+        {
+            var targetCSharp = new QuickSortCSharp();
+            targetCSharp
+                .DoQuickSortOnAlternative(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 })
+                .Should()
+                .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_FSharp()
+        {
+            var targetFSharp = new QuickSortFSharp();
+            targetFSharp
+                .DoQuickSortOn(ListModule.OfSeq(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 }))
+                .ToList()
+                .Should()
+                .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_FSharp_AlternateVersion()
+        {
+            var targetFSharp = new QuickSortFSharp();
+            targetFSharp
+                .DoQuickSortOnAlternative(ListModule.OfSeq(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 }))
+                .ToList()
+                .Should()
+                .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+
     }
 }
---
+++ AFewSimpleThingsFirst/CSharpImplementations/QuickSortCSharp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AFewSimpleThingsFirst
+{
+    public class QuickSortCSharp
+    {
+        public List<int> DoQuickSortOn(List<int> list)
+        {
+            if(list.Count == 0)
+            {
+                return new List<int>();
+            }
+            int elementZero = list[0];
+            List<int> smallerElements = new List<int>();
+            List<int> largerElements = new List<int>();
+            for(int counter = 1; counter < list.Count; counter++)
+            {
+                if(list[counter] < elementZero)
+                {
+                    smallerElements.Add(list[counter]);
+                }
+                if(list[counter] >= elementZero)
+                {
+                    largerElements.Add(list[counter]);
+                }
+            }
+            List<int> result = new List<int>();
+            result.AddRange(DoQuickSortOn(smallerElements));
+            result.Add(elementZero);
+            result.AddRange(DoQuickSortOn(largerElements));
+            return result;
+        }
+
+        public static IEnumerable<int> DoQuickSortOnAlternative(this IEnumerable<int> source)
+        {
+            if(!source.Any())
+            {
+                return new List<int>();
+            }
+
+            int elementZero = source.First();
+            var otherElements = source.Skip(1);
+
+            var smallerElements = otherElements.Skip(1)
+                                        .Where(f => f < elementZero)
+                                        .DoQuickSortOnAlternative();
+            var biggerElements = otherElements.Skip(1)
+                                        .Where(f => f >= elementZero)
+                                        .DoQuickSortOnAlternative();
+
+            List<int> result = new List<int>();
+            result.AddRange(DoQuickSortOnAlternative(smallerElements));
+            result.Add(elementZero);
+            result.AddRange(DoQuickSortOnAlternative(biggerElements));
+            return result;
+        }
+    }
+}
