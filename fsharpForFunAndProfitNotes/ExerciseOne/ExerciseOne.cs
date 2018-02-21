739d1f8fdd6dced298c200efeaafb1cde8de7af1
GitInMaRepo <oliver.s.roth@gmail.com>
2018-02-21 07:51:07 +0100

    Tests for session 2

---
+++ fsharpForFunAndProfitNotes/ExerciseOne/ExerciseOne.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Microsoft.FSharp.Collections;
+using FluentAssertions;
+
+namespace ExerciseOne
+{
+    [TestClass]
+    public class ExerciseOne
+    {
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_CSharp()
+        {
+            var targetCSharp = new QuickSortCSharp();
+            targetCSharp
+                .DoQuickSortOn(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 })
+                .Should()
+                .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+
+        [TestMethod]
+        public void AQuickSortOnARangeOfValues_FSharp()
+        {
+            //var targetFSharp = new QuickSortFSharp();
+            //targetFSharp
+            //    .DoQuickSortOn(ListModule.OfSeq(new List<int> { 1, 7, 3, 15, 54, 12, 2, 5, 9 }))
+            //    .ToList()
+            //    .Should()
+            //    .BeEquivalentTo(new List<int> { 1, 2, 3, 5, 7, 9, 12, 15, 54 });
+        }
+    }
+}
---
+++ fsharpForFunAndProfitNotes/ExerciseOne/QuickSortCSharp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExerciseOne
+{
+    internal class QuickSortCSharp
+    {
+        public QuickSortCSharp()
+        {
+        }
+
+        internal object DoQuickSortOn(List<int> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
