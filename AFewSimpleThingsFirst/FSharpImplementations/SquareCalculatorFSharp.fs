a7380578728a428e3f97c6e8335bcb4fa60e3a0e
GitInMaRepo <oliver.s.roth@gmail.com>
2018-02-21 07:36:00 +0100

    I am nuking everything and starting over. All efforts until now are in the first try branch

--- AFewSimpleThingsFirst/FSharpImplementations/SquareCalculatorFSharp.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace FSharpImplementations
-
-type SquareCalculatorFSharp() = 
-    member this.Square input = input * input
-    //member this.SquaresFromOneTo upper = 
-    //    [1..upper] |> List.map (this.Square) |> List.sum
-    member this.SquaresFromOneTo upper =
-        List.sum ( List.map ( this.Square ) [1..upper] )
